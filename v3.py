38724c378f88daa1bb0b35ff8bb1c9c1f2e89d74
tan-kun <1712huynhtan@gmail.com>
2024-06-21 16:27:28 +0700

    v4

--- v3.py
+++ v3.py
@@ -321,9 +321,8 @@ class Library:
         print('Acc: %s' % np.mean(acc))
         print('AUC: %s' % np.mean(auc))
 
-
     def echo(self, model):
-        print(datetime.datetime.now(), 'Echo')
+        print(datetime.datetime.now(), 'Echo')  
 
         x = self.train_inputs
         print(model(
@@ -369,3 +368,171 @@ class Project(Library):
 #     self.load_ds()
 #     reload = False
 # print('Created data')
+
+
+
+class V4(Project):
+
+    def get_slices(self, df):
+        x, a, b = df['claim'], df['evidence'], df['label']
+
+        indices = [i for i, (x1, x2) in enumerate(zip(x[:-1], x[1:])) if x1 != x2]
+        indices = [0] + indices + [len(x)]
+
+        slices = [slice(x, y, 1) for x, y in zip(indices[:-1], indices[1:])]
+
+        return slices
+
+    def load_ds(self, train_df_path='politifact_train.csv',
+                test_df_path='politifact_test.csv',
+                device=True):
+        print(datetime.datetime.now(), 'Load dataset')
+
+        cap = self.cap
+
+        print("Capped at", cap)
+
+        df = pd.read_csv(train_df_path)[:cap]
+        test_df = pd.read_csv(test_df_path)[:cap]
+
+        if self.shuffle:
+            df = df.sample(frac=1).reset_index(drop=True)
+        else:
+            self.data['train_slices'] = self.get_slices(df)
+            self.data['test_slices'] = self.get_slices(test_df)
+
+        self.data['train_inputs'] = self.tokenize(df['claim'], df['evidence'])
+        self.data['train_labels'] = self.encode_label(df['label'])
+
+        self.data['test_inputs'] = self.tokenize(test_df['claim'], test_df['evidence'])
+        self.data['test_labels'] = self.encode_label(test_df['label'])
+
+        data = self.data
+
+        if device:
+            try:
+                for k, v in self.data.items():
+                    if 'inputs' in k:
+                        self.data[k] = {a: b.to(self.device) for a, b in v.items()}
+                    elif 'labels' in k:
+                        self.data[k] = torch.tensor(v).type(torch.float).to(self.device)
+            except Exception as e:
+                print(e)
+        return data
+
+    def train(self, model, save='base_model.pth'):
+
+        print(datetime.datetime.now(), 'Train')
+
+        optimizer = AdamW(model.parameters(), lr=self.lr)
+
+        batch_size = self.batch_size
+
+        loss_fn = CrossEntropyLoss()
+
+        for epoch in range(self.epochs):
+            print('======== Epoch {:} / {:} ========'.format(epoch + 1, self.epochs))
+
+            scores = {'auc': [], 'acc': []}
+
+            model.train()
+
+            for data in self.get_data(train=True):
+                model.zero_grad(set_to_none=True)
+
+                logits, y = model.call(data)
+
+                loss = loss_fn(logits, y)
+                loss.backward()
+                optimizer.step()
+
+                pred = logits.cpu() > 0
+                true = y.cpu()
+
+                acc = accuracy_score(true, pred)
+
+                scores['acc'].append(acc)
+
+            print('Acc: %s' % np.mean(scores['acc']))
+
+            print('======== Test ========'.format(epoch + 1, self.epochs))
+            self.test(model)
+
+    def test(self, model_or_path):
+        print(datetime.datetime.now(), 'Test')
+
+        if isinstance(model_or_path, str):
+            model = torch.load(model_or_path)
+        else:
+            model = model_or_path
+
+        model.eval()
+
+        scores = {'auc': [], 'acc': []}
+
+        preds, trues = [], []
+
+        with torch.no_grad():
+            for data in self.get_data(train=False):
+                logits, y = model.call(data)
+
+                pred = logits > 0
+
+                trues.extend(y.cpu().numpy().astype(int))
+                preds.extend(pred.cpu().numpy().astype(int))
+
+        trues, preds = np.array(trues), np.array(preds)
+
+        acc = accuracy_score(trues, preds)
+
+        print('Acc: %s' % np.mean(acc))
+
+    def data_iter_v1(self, x, y, slices):
+        batch = slice(0, 0)
+
+        for s in tqdm(slices):
+
+            if s.stop - batch.start < self.batch_size:
+                batch = slice(batch.start, s.stop)
+            else:
+                b = batch
+                batch = slice(batch.stop, s.stop)
+                yield x['input_ids'][b], x['attention_mask'][b], x['token_type_ids'][b], y[b]
+
+    def data_iter_v2(self, x, y, slices):
+        slices = sklearn.utils.shuffle(slices)[:self.cap]
+        for b in tqdm(slices):
+            yield x['input_ids'][b], x['attention_mask'][b], x['token_type_ids'][b], y[b]
+
+    def echo(self, model):
+        print(datetime.datetime.now(), 'Echo')
+
+        for data in self.get_data(train=True):
+            logits, y = model.call(data)
+            print(logits)
+            break
+
+    def get_data(self, train=True):
+        cap = self.cap
+        batch_size = self.batch_size
+
+        if train:
+            x = {k: v[:cap] for k, v in self.data['train_inputs'].items()}
+            y = self.data['train_labels'][:cap]
+            slices = self.data['train_slices']
+        else:
+            x = {k: v for k, v in self.data['test_inputs'].items()}
+            y = self.data['test_labels']
+            slices = self.data['test_slices']
+
+        return self.data_iter_v2(x, y, slices)
+
+
+# self = V4()
+# self.shuffle = False
+# self.cap = 10000
+# # self.device = "cpu"
+# self.load_ds()
+# self.epochs = 20
+# # self.batch_size = 16
+# # self.train(model)
--- v4.py
+++ v4.py
@@ -71,3 +71,92 @@ class NNModel(nn.Module):
 model = NNModel().to(self.device)
 
 self.echo(model)
+
+class NNModel(nn.Module):
+
+    def set_attn(self, layer, input, output):
+        output = torch.mean(output, 1)
+        self.attn = output
+
+    def set_value(self, layer, input, output):
+        self.value = output
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.bb = BertForSequenceClassification.from_pretrained(
+            "bert-base-uncased",
+            num_labels=1,
+        )
+
+        self.last_attn_layer = (
+            self.bb
+            ._modules['bert']
+            ._modules['encoder']
+            ._modules['layer']
+            ._modules['11']
+            ._modules['attention']
+            ._modules['self']
+        )
+
+        self.last_attn_layer._modules['dropout'].register_forward_hook(self.set_attn)
+        self.last_attn_layer._modules['value'].register_forward_hook(self.set_value)
+
+        self.gat_conv = GATv2Conv(-1, 100, heads=1, edge_dim=1)
+        self.fc = nn.Linear(100, 2)
+
+    def forward(self, *input):
+        self.bb(*input)
+
+        nodes_batch = self.value
+        weights_batch = self.attn
+
+        outputs = []
+
+        input_ids = input[0]
+
+        # first_sep_token_pos = (input_ids == 102).nonzero()[0, 1]
+
+        # bridge = torch.zero(s)
+
+        # nodes_batch[:, s]
+
+        # length = self.max_seq_len
+
+        # for nodes in nodes_batch:
+        #     graph = torch.stack(graph, nodes)
+
+        #     edges = edges + [0:7]
+
+
+        for nodes, weights in zip(nodes_batch, weights_batch):
+
+            topk = torch.topk(weights, 100).values
+            threshold = torch.min(topk)
+
+            edges = weights > threshold
+            edges = edges.nonzero().t()
+
+            row, col = edges
+            weights = weights[row, col]
+
+            print(edges)
+
+            nodes = self.gat_conv(nodes, edges, weights)
+            nodes = nn.ReLU()(nodes)
+
+            nodes = nodes[0]
+            nodes = nodes.unsqueeze(0)
+
+            outputs.append(nodes)
+
+        outputs = torch.cat(outputs)
+
+        outputs = self.fc(outputs)
+
+        return outputs
+
+model = NNModel().to(self.device)
+
+self.echo(model)
