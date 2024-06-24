0031f33db1e8d812177485c03be4afe72e132635
tan-kun <1712huynhtan@gmail.com>
2024-06-25 01:08:32 +0700

    fix bug

--- v6.py
+++ v6.py
@@ -210,7 +210,7 @@ class DataMixin:
 
         return tokens, labels
 
-    # @background(max_prefetch=16)
+    @background(max_prefetch=16)
     def data_iter(self):
         if self.is_train:
             df = self.data['train_pandas']
